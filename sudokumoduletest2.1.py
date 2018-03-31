7064e4fe9669f7947f961a80bab573694378de4d
Lyell Read <lyellread@gmail.com>
2018-03-30 23:32:30 -0700

    Working breakdown function
    Got this module working!

--- sudokumoduletest2.1.py
+++ sudokumoduletest2.1.py
@@ -3,9 +3,9 @@ Guess = [[[1, 2, 3, 6, 8], [1, 3, 8, 9], [2, 3, 6, 8], [1, 2, 6, 9], [1, 2, 3, 9
 
 ## Break the first three items in the Grid list into a separate one (for testing) and then do the same for the next two:
 
-first_Set = Grid[0:2]
-second_Set = Grid[3:5]
-third_Set = Grid[6:8]
+first_Set = Grid[0:3]
+second_Set = Grid[3:6]
+third_Set = Grid[6:9]
 
 ## Define a function that can take these three sets and turn them into [[box1][box2][box3]]
 
@@ -14,10 +14,10 @@ def to_Box (set):
     temp_Box_2 = []
     temp_Box_3 = []
     for set_Line in set:  ## this iterates for each line of the original List
-        temp_Box_1.append(set_Line[0:2])
-        temp_Box_2.append(set_Line[3:5])
-        temp_Box_3.append(set_Line[6:8])
-    return[temp_Box1,temp_Box_2,temp_Box_3]
+        temp_Box_1.append(set_Line[0:3])
+        temp_Box_2.append(set_Line[3:6])
+        temp_Box_3.append(set_Line[6:9])
+    return[temp_Box_1,temp_Box_2,temp_Box_3]
 
 ## Another function to print these lists... just to make understanding easier
 
@@ -33,11 +33,26 @@ grid_Print (Grid, 9)
 
 grid_Out = []
 
-for x in to_Box (first_Set)):
-    grid_Out.append(x)
-for x in to_Box (_Set)):
-    grid_Out.append(x)
-for x in to_Box (first_Set)):
-    grid_Out.append(x)
+temp_List = []
+for x in to_Box (first_Set):
+    for y in x:
+        for z in y:
+            temp_List.append(z)
+    grid_Out.append(temp_List)
+    temp_List = []
 
-grid_print (grod_Out, 9)
+for x in to_Box (second_Set):
+    for y in x:
+        for z in y:
+            temp_List.append(z)
+    grid_Out.append(temp_List)
+    temp_List = []
+
+for x in to_Box (third_Set):
+    for y in x:
+        for z in y:
+            temp_List.append(z)
+    grid_Out.append(temp_List)
+    temp_List = []
+
+grid_Print (grid_Out, 9)
